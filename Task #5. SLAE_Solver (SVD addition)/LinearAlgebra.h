436016e0a1cec8526694c9a0220ea580e85a0ceb
Nikita Armbrister <nikritus@gmail.com>
2025-05-03 13:15:52 +0700

    LinearAlgebra.h cosmetic changes

--- Task #5. SLAE_Solver (SVD addition)/LinearAlgebra.h
+++ Task #5. SLAE_Solver (SVD addition)/LinearAlgebra.h
@@ -9,11 +9,11 @@ using Vector = std::vector<double>;
 
 // Matrix operations
 Matrix createMatrix(int N);
-Matrix transpose(const Matrix& A); // Добавлено
-Matrix multiply(const Matrix& A, const Matrix& B); // Добавлено
+Matrix transpose(const Matrix& A);
+Matrix multiply(const Matrix& A, const Matrix& B);
 Vector createRightHandSide(const Matrix& A);
 double computeError(const Vector& x, const Vector& x_exact);
-double computeConditionNumber(const Matrix& A);  // Добавлено
+double computeConditionNumber(const Matrix& A);
 
 // LU decomposition
 void luDecomposition(Matrix& A, std::vector<int>& pivot);
@@ -24,5 +24,6 @@ void householderQR(const Matrix& A, Matrix& Q, Matrix& R);
 Vector solveQR(const Matrix& Q, const Matrix& R, const Vector& f);
 
 // SVD decomposition
-void svdDecomposition(const Matrix& A, Matrix& U, Vector& S, Matrix& V);  // Добавлено
-Vector solveSVD(const Matrix& U, const Vector& S, const Matrix& V, const Vector& f);  // Добавлено
+void computeEigenvalues(const Matrix& A, Vector& eigenvalues, Matrix& eigenvectors);
+void svdDecomposition(const Matrix& A, Matrix& U, Vector& S, Matrix& V);
+Vector solveSVD(const Matrix& U, const Vector& S, const Matrix& V, const Vector& f);
